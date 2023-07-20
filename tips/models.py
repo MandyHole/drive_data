84fde32a660644f3303f44dfa1b829e788353fcf
MandyHole <mandyhole17@gmail.com>
2023-07-20 10:06:20 +0000

    Add Cloudinary link to screenshot default

--- tips/models.py
+++ tips/models.py
@@ -25,7 +25,7 @@ class Tip(models.Model):
     screenshot = models.ImageField(
         upload_to='GetDriveing/',
         verbose_name="Upload a screenshot if applicable",
-        default='../screenshot-default_gbgobl.jpg'
+        default='https://res.cloudinary.com/dd4cchm7g/image/upload/v1689846376/GetDriveing/screenshot-default_gbgobl.jpg'
         blank=True)
     category = models.CharField(
         max_length=32,
