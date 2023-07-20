9b14f580a5b04935e192b3c179cbcb2ad8cbde15
MandyHole <mandyhole17@gmail.com>
2023-07-20 14:58:17 +0000

    Registered Tip to admin

--- tips/admin.py
+++ tips/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Tip
 # Register your models here.
+
+@admin.register(Tip)
+class TipsAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'category', 
+        'owner'
+        )
+    ordering = ['title']
